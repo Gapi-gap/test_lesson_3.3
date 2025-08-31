af1dd221c216d5ea5297895775fe983c50c36d76
artem <artem.gaponov.2024@mail.ru>
2025-08-31 13:46:18 +0300

    First commit

---
+++ src/Bank.java
@@ -0,0 +1,8 @@
+public class Bank {
+    int calculate(int price, double percent, int LifeTime){
+        double StafMother = percent/100/12;
+        int Mother = LifeTime*12;
+        double sum = price *(StafMother * Math.pow(1+StafMother,Mother))/(Math.pow(1+StafMother,Mother)-1);
+        return (int)sum;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Bank service = new Bank();
+        int price = 1_000_000;
+        double percent = 9.99;
+        int LifeTime = 1;
+
+        int result = service.calculate(price, percent, LifeTime); // должно получиться 500
+        System.out.println(result);
+
+         LifeTime = 2;
+        result = service.calculate(price, percent, LifeTime); // должно получиться 500
+        System.out.println(result);
+
+         LifeTime = 3;
+         result = service.calculate(price, percent, LifeTime); // должно получиться 500
+        System.out.println(result);
+    }
+}
