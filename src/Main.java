5e4e7662e54bb27c6981ff51ab7a0a497c8454f0
Gapi-gap <156783799+Gapi-gap@users.noreply.github.com>
2025-08-31 20:46:41 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,17 @@ public class Main {
         Bank service = new Bank();
         int price = 1_000_000;
         double percent = 9.99;
-        int LifeTime = 1;
+        int lifeTime = 1;
 
-        int result = service.calculate(price, percent, LifeTime); // должно получиться 500
+        int result = service.calculate(price, percent, lifeTime); // должно получиться 500
         System.out.println(result);
 
-        LifeTime = 2;
-        result = service.calculate(price, percent, LifeTime); // должно получиться 500
+        lifeTime = 2;
+        result = service.calculate(price, percent, lifeTime); // должно получиться 500
         System.out.println(result);
 
-        LifeTime = 3;
-        result = service.calculate(price, percent, LifeTime); // должно получиться 500
+        lifeTime = 3;
+        result = service.calculate(price, percent, lifeTime); // должно получиться 500
         System.out.println(result);
     }
 }
