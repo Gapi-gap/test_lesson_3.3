7dd1e455e484ce2b6f1a3022cee8a5ca5980d0e7
Gapi-gap <156783799+Gapi-gap@users.noreply.github.com>
2025-08-31 20:46:01 +0300

    Update Bank.java

--- src/Bank.java
+++ src/Bank.java
@@ -1,7 +1,7 @@
 public class Bank {
-    int calculate(int price, double percent, int LifeTime) {
-        double StafMother = percent / 100 / 12;
-        int Mother = LifeTime * 12;
+    int calculate(int price, double percent, int lifeTime) {
+        double stafMother = percent / 100 / 12;
+        int mother = LifeTime * 12;
         double sum = price * (StafMother * Math.pow(1 + StafMother, Mother)) / (Math.pow(1 + StafMother, Mother) - 1);
         return (int) sum;
     }
